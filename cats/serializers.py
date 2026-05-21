ac0f30f7e2c3b2fc909846c0d4976a42652ccd4a
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-21 21:04:35 +0300

    Add uniq validator name and owner for Cat.

--- cats/serializers.py
+++ cats/serializers.py
@@ -33,6 +33,12 @@ class CatSerializer(serializers.ModelSerializer):
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
                   'age')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Cat.objects.all(),
+                fields=('name', 'owner'),
+            ),
+        ]
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
